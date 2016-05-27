d83840de3ce32b0682b71a399f30fa02a7db210d
Hiren Joshi <hirenj@gmail.com>
2016-05-27 10:06:34 +0200

    Add in interface to add user functions

---
+++ test/user-functions.js
@@ -0,0 +1,23 @@
+var transform = require('..');
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+
+var cities = [
+  { foo: 'bar', name: "London", "population": 8615246 },
+  { fod: 'baz', name: "Berlin", "population": 3517424 },
+  { fod: 'bar', name: "Outer London", "population" : "Me"},
+  { name: "Madrid", "population": 3165235 },
+  { name: "Rome",   "population": 2870528 }
+];
+
+var environ = {
+  'toupper' : function(x) { return x.toUpperCase(); }
+};
+
+describe('User functions', function() {
+  it('can run a custom function on the data',function(){
+    var result = transform(cities, { 'value': [ '$..[(toupper(@.name))]' ] }, environ );
+    expect(result.value).have.members( cities.map(function(x) {  return x.name.toUpperCase(); }));
+  });
+});
