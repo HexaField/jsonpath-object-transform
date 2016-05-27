7f5f95020112885aa99baf1c3a5f1d61ffd75182
Hiren Joshi <hirenj@gmail.com>
2016-05-27 08:31:40 +0200

    Add in tests for union mapping

---
+++ test/union-mapping.js
@@ -0,0 +1,23 @@
+var transform = require('..');
+var chai = require('chai');
+var expect = chai.expect;
+
+var cities = [
+  { foo: 'bar', name: "London", "population": 8615246 },
+  { fod: 'baz', name: "Berlin", "population": 3517424 },
+  { fod: 'bar', name: "Outer London", "population" : "Me"},
+  { name: "Madrid", "population": 3165235 },
+  { name: "Rome",   "population": 2870528 }
+];
+
+describe('Union mapping', function() {
+	it('extracts a key from one of many attributes selected using an or',function(){
+		var result = transform(cities, { '$..[(@.foo || @.fod)]' : { 'name' : '@name' } } );
+		expect(result).have.keys(['bar','baz']);
+	});
+	it('extracts a key from attributes subset using array slice',function(){
+		var result = transform(cities, { '$[1,0].name' : { 'name' : '@name' } } );
+		expect(result).have.keys(['Berlin','London']);
+	});
+
+});
