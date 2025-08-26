7ca2fe01722919bc4113937d4f51ac46f05fff69
HexaField <10372036+HexaField@users.noreply.github.com>
2025-08-26 13:30:28 +1000

    cleanup

--- index.js
+++
@@ -1,6 +0,0 @@
-/*jshint node:true*/
-
-// Re-export compiled TypeScript bundle for CJS consumers
-const bundle = require('./dist/index.js')
-module.exports = bundle.default || bundle
-module.exports.Stream = bundle.Stream || (bundle.default && bundle.default.Stream)
